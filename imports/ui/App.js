7a1b1f3e664966b579a2a700c1a3babc78419124
fgerthoffert <f.gerthoffert@gmail.com>
2018-04-05 22:00:57 -0400

    Started integration of Apollo

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -4,6 +4,15 @@ import { withTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloLink, concat } from 'apollo-link';
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context';
+
 import PropTypes from 'prop-types'
 import autoBind from 'react-autobind';
 
@@ -13,6 +22,27 @@ import Index from './Index.js';
 
 import Public from './components/Public/Public.js'
 import Authenticated from './components/Authenticated/Authenticated.js'
+
+
+// Configure Apollo link
+const httpLink = new HttpLink({ uri: 'https://api.github.com/graphql' });
+const authMiddleware = new ApolloLink((operation, forward) => {
+    // add the authorization to the headers
+
+    const githubToken = localStorage.getItem('token');
+    operation.setContext({
+        headers: {
+            authorization: token ? `Bearer ${githubToken}` : "",
+        }
+    });
+
+    return forward(operation);
+})
+const client = new ApolloClient({
+    link: concat(authMiddleware, httpLink),
+    cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
+});
+
 
 class App extends Component {
     constructor(props) {
@@ -28,17 +58,19 @@ class App extends Component {
     render() {
         const { props, state, setAfterLoginPath } = this;
         return (
-            <Router>
-                {!props.loading ? (
-                    <div className="App">
-                        <Switch>
-                            <Route exact name="index" path="/" component={Index} />
-                            <Public path="/login" component={Login} {...props} {...state} />
-                            <Authenticated exact path="/dashboard" component={Dashboard} setAfterLoginPath={setAfterLoginPath} {...props} {...state} />
-                        </Switch>
-                    </div>
-                ) : ''}
-            </Router>
+            <ApolloProvider client={client}>
+                <Router>
+                    {!props.loading ? (
+                        <div className="App">
+                            <Switch>
+                                <Route exact name="index" path="/" component={Index} />
+                                <Public path="/login" component={Login} {...props} {...state} />
+                                <Authenticated exact path="/dashboard" component={Dashboard} setAfterLoginPath={setAfterLoginPath} {...props} {...state} />
+                            </Switch>
+                        </div>
+                    ) : ''}
+                </Router>
+            </ApolloProvider>
         );
     }
 }
--- imports/ui/components/AppMenu/UserAvatar.js
+++ imports/ui/components/AppMenu/UserAvatar.js
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import Avatar from 'material-ui/Avatar';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+//import GQL_GET_USER from './../../../graphql/getUser.graphql';
 
 const styles = {
     row: {
@@ -12,6 +16,7 @@ const styles = {
 
 function UserAvatar(props) {
     const { classes } = props;
+    console.log(props);
     return (
         <div className={classes.root}>
             <Avatar alt={Meteor.user().services.github.username} src="https://avatars3.githubusercontent.com/u/5667028?s=40&v=4" className={classes.avatar} />
@@ -23,4 +28,37 @@ UserAvatar.propTypes = {
     classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(UserAvatar);
+const GET_USER_DATA = gql`
+    query {
+      rateLimit {
+        limit
+        cost
+        remaining
+        resetAt
+      }
+      user(login: "fgerthoffert") {
+        avatarUrl
+        name
+        login
+      }
+    }
+`;
+
+const withData = graphql(GET_USER_DATA, {
+    // destructure the default props to more explicit ones
+    props: ({ data: { error, loading, user, refetch } }) => {
+        console.log(user);
+        console.log(error);
+        console.log(loading);
+        console.log(refetch);
+        if (loading) return { userLoading: true };
+        if (error) return { hasErrors: true };
+
+        return {
+            currentUser: user,
+            refetch,
+        };
+    },
+});
+
+export default withData(withStyles(styles)(UserAvatar));
