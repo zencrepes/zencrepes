8c11c318cc0f41b3a7241b46b35438cf915292b6
fgerthoffert <f.gerthoffert@gmail.com>
2018-10-29 22:23:38 -0400

    .storybook/

--- imports/ui/layouts/General/index.js
+++ imports/ui/layouts/General/index.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
-
 import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
 
-const styles = theme => ({
-    root: {
-        /*
-        flexGrow: 1,
-        zIndex: 1,
-        overflow: 'hidden',
-        position: 'relative',
-        display: 'flex',
-        */
-    },
-});
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import styles from "./styles.jsx";
 
 class General extends Component {
     constructor (props) {
@@ -25,7 +22,26 @@ class General extends Component {
         const { classes, children } = this.props;
         return (
             <div className={classes.root}>
-                {children}
+                <Grid
+                    container
+                    direction="column"
+                    justify="flex-start"
+                    alignItems="flex-start"
+                >
+                    <Grid item xs={12} sm className={classes.fullWidth}>
+                        <AppBar position="static">
+                            <Toolbar>
+                                <Typography variant="h6" color="inherit" className={classes.grow}>
+                                    zCrepes
+                                </Typography>
+                                <Button color="inherit">Login</Button>
+                            </Toolbar>
+                        </AppBar>
+                    </Grid>
+                    <Grid item xs={12} sm className={classes.fullWidth}>
+                        {children}
+                    </Grid>
+                </Grid>
             </div>
         );
     }
