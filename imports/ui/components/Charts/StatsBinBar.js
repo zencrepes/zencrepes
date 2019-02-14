ebbebf523870c9287d3f884768ea86cd41d46a64
fgerthoffert <f.gerthoffert@gmail.com>
2019-02-14 00:04:07 -0500

    Added bins

--- imports/ui/components/Charts/StatsBinBar.js
+++ imports/ui/components/Charts/StatsBinBar.js
@@ -55,7 +55,7 @@ class StatsBinBar extends Component {
                 }
             },
             series: [{
-                name: 'Serie',
+                name: 'Issues',
                 showInLegend: false,
                 data: dataset.map((cat) => {
                     return {
