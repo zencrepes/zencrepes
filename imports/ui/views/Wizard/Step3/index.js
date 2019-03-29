3a81f4287e7297d302f1e0085dff0cf1813d95eb
fgerthoffert <f.gerthoffert@gmail.com>
2019-03-29 11:09:52 -0400

    Fixed linting issue

--- imports/ui/views/Wizard/Step3/index.js
+++ imports/ui/views/Wizard/Step3/index.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import {connect} from "react-redux";
-import PropTypes from "prop-types";
 
 import DataFetch from '../../../components/Settings/Repositories/DataFetch/index.js';
 
